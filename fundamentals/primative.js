98636d146175ec9b13b44af60b91db70427ee546
Jenn <jenn.a.martinez@outlook.com>
2019-10-09 12:04:53 -0700

    adds files.

---
+++ fundamentals/primative.js
@@ -0,0 +1,8 @@
+// primative data types: numbers, booleans, strings, null, undefined
+let name = 'Snoopy';
+let age = '1';
+console.log(age);
+console.log(name);
+
+
+// Reference data types: objects, arrays, functions 
